2a0481d47d9c58d215154bae114068d6f48ab138
Kirthi Shetty <147197142+rustyonkeys@users.noreply.github.com>
2025-12-19 14:28:08 +0530

    Refactor smoothie order submission logic

--- smoothie_app.py
+++ smoothie_app.py
@@ -32,18 +32,14 @@ if ingredients_list:
     smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
     sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width =True)
 
-    # st.write(ingredients_string)
 
-    my_insert_stmt = """insert into smoothies.public.orders(ingredients,name_on_order)
-    values ('""" +ingredients_string+"""','""" +name_on_order+"""')
-    """
+  my_insert_stmt = """insert into smoothies.public.orders(ingredients,name_on_order)
+  values ('""" +ingredients_string+"""','""" +name_on_order+"""')"""
+  time_to_insert = st.button('Submit Order')
 
-
-    time_to_insert = st.button('Submit Order')
-
-    if time_to_insert:
-        session.sql(my_insert_stmt).collect()
-        st.success('Your Smoothie is ordered,' +name_on_order+'!', icon="✅")
+  if time_to_insert:
+    session.sql(my_insert_stmt).collect()
+    st.success('Your Smoothie is ordered,' +name_on_order+'!', icon="✅")
     
         
 
