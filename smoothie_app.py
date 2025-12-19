58abaf8cf13b4905d35c0a17575795eb26865632
Kirthi Shetty <147197142+rustyonkeys@users.noreply.github.com>
2025-12-19 15:09:57 +0530

    Fix formatting of subheader for nutrition information

--- smoothie_app.py
+++ smoothie_app.py
@@ -36,7 +36,7 @@ if ingredients_list:
     ingredients_string += fruit_choosen+ ' '
     search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_choosen, 'SEARCH_ON'].iloc[0]
     st.write('The search value for ', fruit_choosen,' is ', search_on, '.')
-    st.subheader(fruit_choosen + 'Nutrition Information')
+    st.subheader(fruit_choosen + ' Nutrition Information')
     smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")
     sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width =True)
 
